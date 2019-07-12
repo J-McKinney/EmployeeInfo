2672081e2ba5ef07bf1eccbcddf8d81fe9a09ab9
Jesse McKinney <jessman51386@gmail.com>
2019-07-11 20:18:52 -0400

    yay

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-// var config = {
-//     apiKey: "AIzaSyDoMCvEYp-SmRjzLbIt_3JcnsSK02BPVUM",
-//     authDomain: "employeeinfo-bcec0.firebaseapp.com",
-//     databaseURL: "https://employeeinfo-bcec0.firebaseio.com",
-//     projectId: "employeeinfo-bcec0",
-//     storageBucket: "employeeinfo-bcec0.appspot.com",
-//     messagingSenderId: "439651256996",
-//     appId: "1:439651256996:web:cee2a882c0b397ef"
-// };
-// var database = firebase.database();
-// // Initialize Firebase
-// firebase.initializeApp(config);
+var config = {
+    apiKey: "AIzaSyDoMCvEYp-SmRjzLbIt_3JcnsSK02BPVUM",
+    authDomain: "employeeinfo-bcec0.firebaseapp.com",
+    databaseURL: "https://employeeinfo-bcec0.firebaseio.com",
+    projectId: "employeeinfo-bcec0",
+    storageBucket: "employeeinfo-bcec0.appspot.com",
+    messagingSenderId: "439651256996",
+    appId: "1:439651256996:web:9aa0656d1df3c186"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(config);
+  var database = firebase.database();
 
 $("#addEmployeeBtn").on("click", function (event) {
     event.preventDefault();
